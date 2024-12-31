f6bed3fc0bbbe76c60067fec9f64c4fdb21a7597
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2024-12-31 06:51:50 +0100

    tiny fixes

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nix Flakes and Dotfiles
+
+## Linux
+
+```bash
+sudo nixos-rebuild switch --flake .#x86_64-linux
+```
+
+## MacOS (To be tested)
+
+
+
+
--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
     jq
     yq
     nerd-fonts.ubuntu-mono
+    tree
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
