091eebee047f109835660172d0f4bbb0f995eebb
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2025-05-15 12:54:20 +0200

    simplify flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    darwin = {
+    nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -13,12 +13,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, darwin, nix-homebrew, home-manager, nixpkgs, nixvim, ... }@inputs:
+  outputs = { self, nix-darwin, nix-homebrew, home-manager, nixpkgs, nixvim, ... }@inputs:
     let
       username = "tim";
-      linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
-      darwinSystems = [ "aarch64-darwin" "x86_64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) f;
       devShell = system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in {
@@ -35,10 +32,8 @@
     in {
       # devShells = forAllSystems devShell;
 
-      darwinConfigurations = nixpkgs.lib.genAttrs darwinSystems (system:
-        darwin.lib.darwinSystem {
-          inherit system;
-          specialArgs = inputs;
+      darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
           modules = [
             home-manager.darwinModules.home-manager
             {
@@ -53,27 +48,24 @@
             ./hosts/common
             ./hosts/darwin
           ];
-        });
+        };
 
-      nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = inputs;
-          modules = [
-            home-manager.nixosModules.home-manager
-            {
-              users.users.${username}.home = "/home/${username}";
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users."${username}" = import ./home.nix;
-                extraSpecialArgs = { inherit username nixvim; };
-              };
-            }
-            ./hosts/common
-            ./hosts/nixos
-          ];
-        });
-      
+      nixosConfigurations."server" = nixpkgs.lib.nixosSystem { 
+        system = "x86_64-linux";
+        modules = [
+          home-manager.nixosModules.home-manager
+          {
+            users.users.${username}.home = "/home/${username}";
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users."${username}" = import ./home.nix;
+              extraSpecialArgs = { inherit username nixvim; };
+            };
+          }
+          ./hosts/common
+          ./hosts/nixos
+        ];
+      };
     };
 }
