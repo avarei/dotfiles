db54e54d72f05ce2a7373f458371272ec9ce4f8d
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2025-12-02 21:48:30 +0100

    neovim: make nvf config good

--- home/editor/nvf.nix
+++ home/editor/nvf.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, lib, nvf, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  nvf,
+  ...
+}: {
   imports = [
     nvf.homeManagerModules.default
   ];
@@ -9,12 +13,10 @@
     packages = with pkgs; [
       # delve
       # gopls
-      # go
+      go
       # vimPlugins.nvim-treesitter.builtGrammars.go
-
     ];
   };
-  
 
   programs.nvf = {
     enable = true;
@@ -22,7 +24,6 @@
       vim = {
         options = {
           mouse = "a";
-
 
           # Tab
           tabstop = 2; # number of visual spaces per TAB
@@ -46,9 +47,18 @@
           registers = "unnamedplus";
         };
         # clipboard.providers = {  }
-
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+          inlayHints.enable = true;
+        };
         languages = {
-          nix.enable = true;
+          nix = {
+            enable = true;
+            format.enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
           go = {
             enable = true;
             dap.enable = true;
@@ -81,14 +91,46 @@
           style = "mocha";
         };
         visuals.nvim-web-devicons.enable = true;
-        statusline.lualine.enable = true;
+        statusline.lualine = {
+          enable = true;
+          theme = "catppuccin";
+        };
         notes.todo-comments.enable = true;
         utility.surround.enable = true;
-        
-        # autocomplete.blink-cmp.enable = true;
+        utility.snacks-nvim.enable = true;
+        binds.whichKey.enable = true;
+        git = {
+          enable = true;
+          git-conflict.enable = true;
+        };
 
+        telescope.enable = true;
+
+        autocomplete.nvim-cmp.enable = true;
+        dashboard.startify = {
+          enable = true;
+          changeToVCRoot = true;
+        };
+        diagnostics = {
+          enable = true;
+          config = {
+            signs.text = lib.generators.mkLuaInline ''
+              {
+                [vim.diagnostic.severity.ERROR] = "󰅚 ",
+                [vim.diagnostic.severity.WARN] = "󰀪 ",
+              }
+            '';
+            virtual_lines = true;
+          };
+        };
+        autopairs.nvim-autopairs.enable = true;
       };
-
+      tabline = {
+        nvimBufferline.enable = true;
+      };
+      notify = {
+        nvim-notify.enable = true;
+      };
     };
   };
 }
