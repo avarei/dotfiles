ed138d43ff5e7758ae485672ce29d1cf0b6c7f68
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2025-12-07 00:05:55 +0100

    git: dynamic identities based on remote; gpg: add new key

--- home/git.nix
+++ home/git.nix
@@ -4,24 +4,46 @@
       git
     ];
   };
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
   programs.git = {
     enable = true;
-    userName = "Tim Geimer";
-    userEmail = "32556895+Avarei@users.noreply.github.com";
 
-    extraConfig = {
+    settings = {
+      user.name = "Tim Geimer";
+      user.email = "32556895+Avarei@users.noreply.github.com";
+      user.signingkey = "F47E71666F3E317F";
       credential = {
         helper = "cache --timeout=36000";
       };
-      commit.gpgSign = true;
-      user.signingkey = "F47E71666F3E317F";
+      commit.gpgSign = false;
 
       init = {defaultBranch = "main";};
       push = {autoSetupRemote = true;};
     };
-
-    delta = {
-      enable = true;
-    };
+    includes = [
+      {
+        condition = "hasconfig:remote.*.url:git@github.com:*/**";
+        contents = {
+          user = {
+            name = "Tim Geimer";
+            email = "32556895+Avarei@users.noreply.github.com";
+          };
+          commit.gpgSign = true;
+        };
+      }
+      {
+        condition = "hasconfig:remote.*.url:git@gitlab.opencode.de:*/**";
+        contents = {
+          user = {
+            name = "Tim Geimer";
+            email = "3679-tim.geimer@users.noreply.gitlab.opencode.de";
+          };
+          commit.gpgSign = true;
+        };
+      }
+    ];
   };
 }
--- home/gpg.nix
+++ home/gpg.nix
@@ -50,32 +50,36 @@
           Lm5vcmVwbHkuZ2l0aHViLmNvbT6IlAQTFgoAPBYhBBDsKxTc6NUcwrNuWvo2creO
           hYErBQJpMj+BAhsDBQkSzAMABAsJCAcEFQoJCAUWAgMBAAIeAQIXgAAKCRD6NnK3
           joWBKz8pAQDRWKgICEjsZaU59Uz77Tk8ZE5KegxPQg9+TSkusBs/YwD/SRX+GxQ5
-          VuKqRUNIFglRnOVzVZ79psDUXUZ5UIKiSg24MwRpMj+qFgkrBgEEAdpHDwEBB0Dc
-          gBToKJ7K0cI+Kpi98ij0dWjHgG/fSNW+vE+bjwbY8Ij1BBgWCgAmFiEEEOwrFNzo
-          1RzCs25a+jZyt46FgSsFAmkyP6oCGwIFCQPCZwAAgQkQ+jZyt46FgSt2IAQZFgoA
-          HRYhBP09h/VNGciz4aS4NfR+cWZvPjF/BQJpMj+qAAoJEPR+cWZvPjF/xfUA/2eR
-          byw9ckxgVU41/vahs8JFnkM91Jn/Gs3fURHfvRmDAP99yWnuXqYeqBkf9/T2aHEQ
-          l2oGosZLiz9oul6KHNHkDd3VAP4hWcyP9UjW/YZM4RxJEoeqkrV5EMY+5IwBCJIs
-          ckO4ugEA6el1n2Fgtlqb2Pyt1gD6yMee3B3AMz3BljIEtltWFwi4MwRpMj/CFgkr
-          BgEEAdpHDwEBB0BY2pZ7F0778C9Fw39tVb9qBwPQfcCG/x/DSX2xF9qRSoh+BBgW
-          CgAmFiEEEOwrFNzo1RzCs25a+jZyt46FgSsFAmkyP8ICGyAFCQPCZwAACgkQ+jZy
-          t46FgSuHiAD8C9PSoOeeY0YLSTrBCmAZYeh2MgtegnO47nR+2nptGJwBANluQxNi
-          IC5xVsXqIH6q/zfBO7E/XQvWuWPM0IcsQM4FuQINBGkyP/8BEADUVu4uzkIdd4JL
-          bJ9jcABu5d408821XV1jOG8QjPwrlABtlsimNF3YyjkCORyD8A/X0bA/EpvCUykI
-          T7nCv2oqbOT0V3hIHG54t55XfWwuCERMjrJgK+iU7CHq0zrziD+Imv9O+kum4+/M
-          kUc63JJNurY+/uwL73h4+6Hrx/h5DorY6oJrqYizBAwVDaxt8BZnoRXN21YoVwUH
-          BJZ0I9Gzr3h/CCIBAzFWPnWNuUr89Oq3KNdmyRKJHJTCTY5eBb3MSfo5qAFn7vIQ
-          TU+yU0MTa6RgTPfH35mni+/GXm6dTrIGoyKWzuIJaLQ9JT7XvbtMkOxzppZqFto5
-          mX0yprqe8pu65Gkn67TWji96vXsrdwmRiuK0db2fBA1Y28MteFXwga9VMyRfpxBh
-          xKW/Q7oNowDQpYvAmcYkZgn+xLU1+vMa9subIuEiapQxB4QxA7IPl/E/cckTKNWd
-          aMGaYW5dFVtdYopkLhIdvOLGGcJI2qYDcBYtcnfsm+IYCawqCKdely32Te9oR9EO
-          DqHWvFc4W2jSS+Ku/AisdydpSKXXmokUM3Y3BLoEmbffncmVbmqE7OzdVFr3OOR1
-          k5RnhgUGwElzSyr6qNZXK8I80UvcHH9LIvhh1DfQ3NJqDHrBvDPP1+X8qOmmh+CN
-          E1uH6J2+D0YGZGXojJ3F+q5IW5tM1wARAQABiH4EGBYKACYWIQQQ7CsU3OjVHMKz
-          blr6NnK3joWBKwUCaTI//wIbDAUJA8JnAAAKCRD6NnK3joWBK4D+AP9YAdOGeEy5
-          y09taM0x17RjgpdjrHKiuDG155ZG9lW/qQEAhv2TcD3An0o4ZmPZWAHtM9UJT1yD
-          yIgSYXKy3DjM/AM=
-          =aXmh
+          VuKqRUNIFglRnOVzVZ79psDUXUZ5UIKiSg20PVRpbSBHZWltZXIgPDM2NzktdGlt
+          LmdlaW1lckB1c2Vycy5ub3JlcGx5LmdpdGxhYi5vcGVuY29kZS5kZT6IlAQTFgoA
+          PBYhBBDsKxTc6NUcwrNuWvo2creOhYErBQJpMzGNAhsDBQkSzAMABAsJCAcEFQoJ
+          CAUWAgMBAAIeAQIXgAAKCRD6NnK3joWBK4UkAQCdKxFzIQTtzYZEgk1Oa/VnkDRf
+          x/bbVLnV9xQUpvpFSAEAlmUeBwuBDUyrOAhmjkY8Jna13uDiV64Aoiv3hR1FvwS4
+          MwRpMj+qFgkrBgEEAdpHDwEBB0DcgBToKJ7K0cI+Kpi98ij0dWjHgG/fSNW+vE+b
+          jwbY8Ij1BBgWCgAmFiEEEOwrFNzo1RzCs25a+jZyt46FgSsFAmkyP6oCGwIFCQPC
+          ZwAAgQkQ+jZyt46FgSt2IAQZFgoAHRYhBP09h/VNGciz4aS4NfR+cWZvPjF/BQJp
+          Mj+qAAoJEPR+cWZvPjF/xfUA/2eRbyw9ckxgVU41/vahs8JFnkM91Jn/Gs3fURHf
+          vRmDAP99yWnuXqYeqBkf9/T2aHEQl2oGosZLiz9oul6KHNHkDd3VAP4hWcyP9UjW
+          /YZM4RxJEoeqkrV5EMY+5IwBCJIsckO4ugEA6el1n2Fgtlqb2Pyt1gD6yMee3B3A
+          Mz3BljIEtltWFwi4MwRpMj/CFgkrBgEEAdpHDwEBB0BY2pZ7F0778C9Fw39tVb9q
+          BwPQfcCG/x/DSX2xF9qRSoh+BBgWCgAmFiEEEOwrFNzo1RzCs25a+jZyt46FgSsF
+          AmkyP8ICGyAFCQPCZwAACgkQ+jZyt46FgSuHiAD8C9PSoOeeY0YLSTrBCmAZYeh2
+          MgtegnO47nR+2nptGJwBANluQxNiIC5xVsXqIH6q/zfBO7E/XQvWuWPM0IcsQM4F
+          uQINBGkyP/8BEADUVu4uzkIdd4JLbJ9jcABu5d408821XV1jOG8QjPwrlABtlsim
+          NF3YyjkCORyD8A/X0bA/EpvCUykIT7nCv2oqbOT0V3hIHG54t55XfWwuCERMjrJg
+          K+iU7CHq0zrziD+Imv9O+kum4+/MkUc63JJNurY+/uwL73h4+6Hrx/h5DorY6oJr
+          qYizBAwVDaxt8BZnoRXN21YoVwUHBJZ0I9Gzr3h/CCIBAzFWPnWNuUr89Oq3KNdm
+          yRKJHJTCTY5eBb3MSfo5qAFn7vIQTU+yU0MTa6RgTPfH35mni+/GXm6dTrIGoyKW
+          zuIJaLQ9JT7XvbtMkOxzppZqFto5mX0yprqe8pu65Gkn67TWji96vXsrdwmRiuK0
+          db2fBA1Y28MteFXwga9VMyRfpxBhxKW/Q7oNowDQpYvAmcYkZgn+xLU1+vMa9sub
+          IuEiapQxB4QxA7IPl/E/cckTKNWdaMGaYW5dFVtdYopkLhIdvOLGGcJI2qYDcBYt
+          cnfsm+IYCawqCKdely32Te9oR9EODqHWvFc4W2jSS+Ku/AisdydpSKXXmokUM3Y3
+          BLoEmbffncmVbmqE7OzdVFr3OOR1k5RnhgUGwElzSyr6qNZXK8I80UvcHH9LIvhh
+          1DfQ3NJqDHrBvDPP1+X8qOmmh+CNE1uH6J2+D0YGZGXojJ3F+q5IW5tM1wARAQAB
+          iH4EGBYKACYWIQQQ7CsU3OjVHMKzblr6NnK3joWBKwUCaTI//wIbDAUJA8JnAAAK
+          CRD6NnK3joWBK4D+AP9YAdOGeEy5y09taM0x17RjgpdjrHKiuDG155ZG9lW/qQEA
+          hv2TcD3An0o4ZmPZWAHtM9UJT1yDyIgSYXKy3DjM/AM=
+          =1XF6
           -----END PGP PUBLIC KEY BLOCK-----
         '';
         trust = "ultimate";
