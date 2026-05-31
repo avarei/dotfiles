f60c13b4fcf5b71379bc17c5b5be6a69ebfbe88a
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2026-06-01 00:26:34 +0200

    add steamvr

--- modules/nixos/gaming/steam.nix
+++ modules/nixos/gaming/steam.nix
@@ -16,5 +16,25 @@ in {
     };
 
     hardware.xone.enable = true;
+    hardware.steam-hardware.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+      wireplumber.extraConfig."51-index-audio" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {"device.name" = "alsa_card.pci-0000_01_00.1";}
+            ];
+            actions.update-props = {
+              "device.profile" = "output:hdmi-stereo-extra1";
+            };
+          }
+        ];
+      };
+    };
   };
 }
