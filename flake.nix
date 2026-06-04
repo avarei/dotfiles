1506c208ed135f77ec7796e509b5acf188df809f
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2026-06-04 04:50:09 +0200

    Update stylix to 26.05

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
 
     stylix = {
-      url = "github:nix-community/stylix/release-25.11";
+      url = "github:nix-community/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvf = {
